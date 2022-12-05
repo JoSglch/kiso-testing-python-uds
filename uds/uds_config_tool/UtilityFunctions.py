074379456c85e13f692ede5f780f79e1c2b8b8bd
JoSglch <32525495+JoSglch@users.noreply.github.com>
2022-12-05 10:25:03 +0100

    fix

--- uds/uds_config_tool/UtilityFunctions.py
+++ uds/uds_config_tool/UtilityFunctions.py
@@ -263,7 +263,7 @@ def get_diag_coded_type_from_structure(
             logging.info(f"structure: {id}, diag = {diag}, dopId = {dopid}, dop.tag = {tag}")
         # dop is another structure:
         if dop.tag == "STRUCTURE":
-            base_data_type = get_diag_coded_type_from_dop(dop, xml_elements)
+            base_data_type = get_diag_coded_type_from_structure(dop, xml_elements)
             logging.info(f"structure in structure =>  dop.tag = {dop.tag}: {base_data_type}")
         base_data_type = dop.find("DIAG-CODED-TYPE").attrib["BASE-DATA-TYPE"]
         diag_coded_type = StandardLengthType(base_data_type, byte_length)
